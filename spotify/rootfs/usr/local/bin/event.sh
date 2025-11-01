e0754482c931838c31163038a94b970c055799b4
Tobias Koch <koch.tobias92@gmx.de>
2025-11-01 11:14:09 +0100

    event.sh: minor changes, fix in run?

--- spotify/rootfs/usr/local/bin/event.sh
+++ spotify/rootfs/usr/local/bin/event.sh
@@ -1,21 +1,18 @@
-case ${PLAYER_EVENT} in
+#!/bin/bash
 
+case ${PLAYER_EVENT} in
 changed | started | playing | preloading)
   STATE="playing"
   ;;
-
 stopped)
   STATE="idle"
   ;;
-
 paused)
   STATE="paused"
   ;;
-
 sink)
   STATE="sink"
   ;;
-
 volume_set)
   STATE="vol"
   ;;
@@ -28,40 +25,34 @@ if [ -n "${STATE}" ]; then
 
   # sink event
   if [ -n "${SINK_STATUS}" ]; then
-
     curl -s \
       -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
       -H "Content-Type: application/json" \
-      -XPOST http://supervisor/core/api/states/sensor.spotify_addon_sink_state \
+      -X POST http://supervisor/core/api/states/sensor.spotify_addon_sink_state \
       -d "{\"state\":\"${SINK_STATUS}\"}"
 
   # volume event
   elif [ -n "${VOLUME}" ]; then
-
     curl -s \
       -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
       -H "Content-Type: application/json" \
-      -XPOST http://supervisor/core/api/states/sensor.spotify_addon_volume \
+      -X POST http://supervisor/core/api/states/sensor.spotify_addon_volume \
       -d "{\"state\":\"${VOLUME}\"}"
 
   # track id event
   else
-
     curl -s \
       -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
       -H "Content-Type: application/json" \
-      -XPOST http://supervisor/core/api/states/sensor.spotify_addon \
+      -X POST http://supervisor/core/api/states/sensor.spotify_addon \
       -d "{\"state\":\"${STATE}\",\"attributes\":{\"track_id\":\"${TRACK_ID}\",\"timestamp\":\"${TIMESTAMP}\"}}"
-
   fi
 
   if [ -n "${OLD_TRACK_ID}" ]; then
-
     curl -s \
       -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
       -H "Content-Type: application/json" \
-      -XPOST http://supervisor/core/api/states/sensor.spotify_addon_old_track_id \
+      -X POST http://supervisor/core/api/states/sensor.spotify_addon_old_track_id \
       -d "{\"state\":\"${OLD_TRACK_ID}\"}"
-
   fi
 fi
